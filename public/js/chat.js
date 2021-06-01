9b193b3d16480103f2b1cda20e72f0dd202b30d2
Ekant <ekantchandrakar07@gmail.com>
2021-06-01 23:05:04 +0530

    setup the event acknowledgment

--- public/js/chat.js
+++ public/js/chat.js
@@ -8,7 +8,12 @@ document.querySelector("#message-form").addEventListener("submit", (e) => {
   e.preventDefault();
 
   const message = e.target.elements.message.value; // Grab the value property from input
-  socket.emit("sendMessage", message);
+  socket.emit("sendMessage", message, (error) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Message delivered");
+  });
 });
 
 document.querySelector("#send-location").addEventListener("click", () => {
@@ -20,6 +25,8 @@ document.querySelector("#send-location").addEventListener("click", () => {
     socket.emit("sendLocation", {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
+    }, () => {
+      console.log("Location Shared");
     });
   });
 });
