ed76239d3c0946a2f2a67c06af8dab9f33040269
Ekant <ekantchandrakar07@gmail.com>
2021-06-02 23:38:12 +0530

    rendering the location on browser

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,6 +9,7 @@ const $messages = document.querySelector("#messages"); // location to render tem
 
 // Templates
 const messageTemplate = document.querySelector("#message-template").innerHTML;
+const locationMessageTemplate = document.querySelector("#location-message-template").innerHTML;
 
 socket.on("message", (message) => {
   console.log(message);
@@ -16,6 +17,14 @@ socket.on("message", (message) => {
     message: message
   });
   $messages.insertAdjacentHTML('beforeend', html);
+});
+
+socket.on("locationMessage", (url) => {
+  console.log(url);
+  const html = Mustache.render(locationMessageTemplate, {
+    url: url
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
 });
 
 $messageForm.addEventListener("submit", (e) => {
